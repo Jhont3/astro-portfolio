f7e885469da3a3881a2d1dec28c8bc2cced1d7d5
Jhonatan <jhont.escobar17@gmail.com>
2023-05-30 15:52:45 -0500

    base posible animations, position elements and start script ident each img

--- portfolio/tailwind.config.cjs
+++ portfolio/tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				boxing: ['boxing', 'sans-serif'],
+				'boxing-striped': ['Boxing Striped', 'sans-serif'],
+			},
+			colors: {
+				"blue-1": "#003566",
+				"blue-2": "#001d3d",
+				"blue-3": "#000814",
+				"yellow-1": "#ffd60a",
+				"yellow-2": "#ffc300",
+			  },
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated')
+	  ],
 }
